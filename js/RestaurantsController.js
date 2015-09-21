b67cb9630bc6e505ffdc2d2dfcee77967827cda9
Annie Hoogendoorn & Brian Borealis <student@epicodus.com>
2015-09-21 12:00:36 -0700

    Add cuisine type, location and price range.

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -3,9 +3,12 @@ bestRestaurant.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
 
     //restaurantName is defined in the html ng-model directive.
     //After the restaurantName is added to the array, we clear it so it doesn't get added twice
-    $scope.addRestaurants = function () {
-        $scope.restaurants.push({ name: $scope.restaurantName });
+    $scope.addRestaurant = function () {
+        $scope.restaurants.push({ name: $scope.restaurantName, food: $scope.restaurantFood, location: $scope.restaurantLocation, price: $scope.restaurantPrice });
         $scope.restaurantName = null;
+        $scope.restaurantFood = null;
+        $scope.restaurantLocation = null;
+        $scope.restaurantPrice = null;
     };
     $scope.deleteRestaurant = function(restaurant) {
         var index = $scope.restaurants.indexOf(restaurant);
